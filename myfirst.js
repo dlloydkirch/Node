a0d1a1e54275c587e00107a0188633305c6e148f
David Kirch <dlloydkirch@gmail.com>
2018-04-10 20:43:28 -0500

    still in progress

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,10 @@
 var http = require('http');
+var dt = require('./myfirstmodule');
 
 http.createServer(function (req, res){
 	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write("The date and time are currently: " + dt.myDateTime());
+	res.write("<br>");
 	res.end('Hello World!');
 }).listen(8080);
+
